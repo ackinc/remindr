d50a96e4f1a3eb88e700a2768e77336bdc9dd703
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-06-01 00:05:36 +0530

    Bugfix: reset-db script broke because import was no longer configured correctly

--- scripts/reset-db.py
+++ scripts/reset-db.py
@@ -1,8 +1,15 @@
 import os
+import sys
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-from app.models import Base
+# to support absolute import of app.models
+# wish there was an easy way to relative-import it instead,
+#   but there doesn't seem to be
+project_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(project_dir)
+
+from app.models import Base  # noqa: E402
 
 load_dotenv()
 
