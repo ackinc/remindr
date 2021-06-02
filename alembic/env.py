4d9a523792a902b704a522c607604ff5b817fc42
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-06-02 16:58:05 +0530

    Added base metadata to alembic conf

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine, pool
 
 from alembic import context
+from app.models import Base
 
 load_dotenv()
 
@@ -22,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
